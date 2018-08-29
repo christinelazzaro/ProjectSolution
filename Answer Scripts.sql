78a45a6082e7f5f3b34dfd0e54d1369cbd1d6ee0
Christine Lazzaro <christinelazzaro@gmail.com>
2018-08-29 11:03:26 -0500

    Question 2 added
    script for Question 2 of the questionnaire was added to the solution set

--- Answer Scripts.sql
+++ Answer Scripts.sql
@@ -4,3 +4,14 @@ SELECT
 +
 (SELECT count(*) Total FROM mindbody_reservations WHERE reserved_at BETWEEN '2018-01-01' AND '2018-02-28');
     
+### Question2:  Which studio has the highest rate of reservation abandonment (did not cancel but did not check-in)? ###
+SELECT studio_key Studio, count(*) Total FROM clubready_reservations CRRes
+	WHERE CRRes.signed_in_at is null AND CRRes.canceled = "f"
+    GROUP BY Studio
+UNION
+	SELECT studio_key Studio, count(*) Total FROM mindbody_reservations MBRes
+	WHERE MBRes.checked_in_at is null AND MBRes.canceled_at is null
+	GROUP BY Studio 
+ORDER BY Total DESC 
+LIMIT 1;
+
